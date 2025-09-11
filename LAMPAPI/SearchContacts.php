aa262042ac5921a8563075bab780729e77656d78
Paola Bayona <bayonapaola2002@gmail.com>
2025-09-11 16:03:05 -0400

    searchcontacts.php and code.js modified to parse results correctly

--- LAMPAPI/SearchContacts.php
+++ LAMPAPI/SearchContacts.php
@@ -1,60 +1,68 @@
 <?php
 
-	$inData = getRequestInfo();
-	
-	$searchResults = "";
-	$searchCount = 0;
+    $inData = getRequestInfo();
+    
+    $searchResults = array();
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-	if ($conn->connect_error) 
-	{
-		returnWithError( $conn->connect_error );
-	} 
-	else
-	{
-		$sql = "select firstName, lastName, Email, Phone, id from Contacts where firstName like '%" . $inData["firstName"] . "%' and lastName like '%" .$inData["lastName"] . "%' and UserID=" . $inData["userId"];
-		$result = $conn->query($sql);
-		if ($result->num_rows > 0)
-		{
-			while($row = $result->fetch_assoc())
-			{
-				if( $searchCount > 0 )
-				{
-					$searchResults .= ",";
-				}
-				$searchCount++;
-				$searchResults .= '"' . $row["firstName"] . '" , "' .$row["lastName"]. '" , "' . $row["Email"] . '" , "' . $row["Phone"] . '" , "' . $row["id"] . '"';
-			}
-			returnWithInfo( $searchResults );
-		}
-		else
-		{
-			returnWithError( "No Records Found" );
-		}
-		$conn->close();
-	}
+    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+    if ($conn->connect_error) 
+    {
+        returnWithError($conn->connect_error);
+    } 
+    else
+    {
+        // Use LIKE on first and last name, plus filter by UserID
+        $sql = "SELECT firstName, lastName, Email, Phone, ID 
+                FROM Contacts 
+                WHERE firstName LIKE '%" . $inData["firstName"] . "%' 
+                AND lastName LIKE '%" . $inData["lastName"] . "%' 
+                AND UserID=" . $inData["userId"];
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+        $result = $conn->query($sql);
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"results": [], "error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
-	function returnWithInfo( $searchResults )
-	{
-		$retValue = '{"results":[' . $searchResults . '],"error":""}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
+        if ($result->num_rows > 0)
+        {
+            while($row = $result->fetch_assoc())
+            {
+                $searchResults[] = array(
+                    "FirstName" => $row["firstName"],
+                    "LastName"  => $row["lastName"],
+                    "Email"     => $row["Email"],
+                    "Phone"     => $row["Phone"],
+                    "ID"        => $row["ID"]
+                );
+            }
+            returnWithInfo($searchResults);
+        }
+        else
+        {
+            returnWithError("No Records Found");
+        }
+
+        $conn->close();
+    }
+
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+    function sendResultInfoAsJson($obj)
+    {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+    
+    function returnWithError($err)
+    {
+        $retValue = array("results" => [], "error" => $err);
+        sendResultInfoAsJson(json_encode($retValue));
+    }
+    
+    function returnWithInfo($searchResults)
+    {
+        $retValue = array("results" => $searchResults, "error" => "");
+        sendResultInfoAsJson(json_encode($retValue));
+    }
+    
 ?>
